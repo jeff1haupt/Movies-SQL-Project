5c6cce1c5e4c5be6fc14cbd4e90ac0973bd06620
clairgustafson2 <66271868+clairgustafson2@users.noreply.github.com>
2020-08-20 11:47:40 -0700

    Update moviesDB.sql

--- moviesDB.sql
+++ moviesDB.sql
@@ -8,16 +8,18 @@ DROP TABLE IF EXISTS genre;
 
 CREATE table genre(
 id int (11) not null auto_increment,
-genre_name varchar(20) not null,
+genre_name VARCHAR(255) not null,
 PRIMARY KEY (id) 
 );
 
 CREATE table movie(
 Id int (11) not null auto_increment,
-movie_title varchar(25) not null,
-movie_length char(4),
-film_rating char(6),
+movie_title varchar(250) not null,
+movie_length int,
 release_date date,
+director VARCHAR(50),
+lead_actor VARCHAR(50),
+revenue_made VARCHAR(50),
 genre_id int(11) not null,
 PRIMARY KEY (id),
 FOREIGN KEY (genre_id) references genre(id)
@@ -25,7 +27,7 @@ FOREIGN KEY (genre_id) references genre(id)
 
 CREATE table critic_rating(
 id int (11) not null auto_increment,
-star_rating char(5) not null,
+star_rating int not null,
 movie_id int(11) not null,
 PRIMARY KEY (id),
 FOREIGN KEY (movie_id) references movie(id)
