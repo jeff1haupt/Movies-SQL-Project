b2bf4173b724650519e57b819667396ddfa1cd80
clairgustafson2 <66271868+clairgustafson2@users.noreply.github.com>
2020-08-21 16:46:35 -0700

    Update in movieDao

--- src/Dao/MovieDao.java
+++ src/Dao/MovieDao.java
@@ -8,8 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.List;
-
 import Entity.Movie;
 
 
@@ -17,9 +15,9 @@ public class MovieDao {
 	
 	private Connection connection;
 	private final String CREATE_NEW_MOVIE_QUERY = "INSERT INTO movie(String movieTitle, int movieLength, String releaseDate, String director, \n" + 
-			"				String leadActor, String revenue, int genreId) VALUES (?, ?, ?, ?, ?, ?, ?)";
+			"				String leadActor, String revenueMade, int genreId) VALUES (?, ?, ?, ?, ?, ?, ?)";
 	private final String UPDATE_MOVIE_BY_ID_QUERY = "UPDATE movie SET movie_title = ?, movie_length = ?, release_date =?, director =?,"
-			+ "lead_actor= ?, revenue =?, genre_id =? WHERE id =?";
+			+ "lead_actor= ?, revenueMade =?, genre_id =? WHERE id =?";
 	private final String DISPLAY_ALL_MOVIES_QUERY = "SELECT * FROM movie";
 	private final String DELETE_MOVIE_BY_ID_QUERY = "DELETE FROM movie WHERE id = ?";
 	
@@ -29,21 +27,21 @@ public class MovieDao {
 	
 	//1. create a new movie
 		public void createMovie(String movieTitle, int movieLength, String releaseDate, String director, 
-				String leadActor, String revenue, int genreId) throws SQLException {
+				String leadActor, String revenueMade, int genreId) throws SQLException {
 			PreparedStatement ps = connection.prepareStatement(CREATE_NEW_MOVIE_QUERY);
 			ps.setString(1, movieTitle);
 			ps.setInt(2, movieLength);
 			ps.setString(3, releaseDate);
 			ps.setString(4, director);
 			ps.setString(5, leadActor);
-			ps.setString(6, revenue);
+			ps.setString(6, revenueMade);
 			ps.setInt(7, genreId);
 			ps.executeUpdate();
 		}
 		
 	// 2. update a movie
 		public void updateMovie(String movieTitle, int movieLength, String releaseDate, String director, 
-				String leadActor, String revenue, int genreId) throws SQLException {
+				String leadActor, String revenue, int genreId, int id) throws SQLException {
 			PreparedStatement ps = connection.prepareStatement(UPDATE_MOVIE_BY_ID_QUERY);
 			ps.setString(1, movieTitle);
 			ps.setInt(2, movieLength);
@@ -52,6 +50,7 @@ public class MovieDao {
 			ps.setString(5, leadActor);
 			ps.setString(6, revenue);
 			ps.setInt(7, genreId);
+			ps.setInt(8, id);
 			ps.executeUpdate();
 		}
 	
