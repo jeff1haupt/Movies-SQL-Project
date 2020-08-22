d10f7c6bd418d6221240855683c4380281d9f99e
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-08-21 20:05:41 -0400

    Added password request

--- src/Dao/DBConnection.java
+++ src/Dao/DBConnection.java
@@ -3,14 +3,18 @@ package Dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 //import dao.DBConnection;
 
 public class DBConnection {
+	
+	
+	static Scanner scanner = new Scanner(System.in);
 
 	private final static String URL = "jdbc:mysql://localhost:3306/movies";
 	private final static String USERNAME = "root";
-	private final static String PASSWORD = "password";
+	private final static String PASSWORD = getPassword();
 	private static Connection connection;
 	private static DBConnection instance;
 	
@@ -18,6 +22,12 @@ public class DBConnection {
 		DBConnection.connection = connection;
 	}
 	
+	private static String getPassword() {
+		System.out.println("Enter the database password: ");
+		String userPassword = scanner.nextLine();
+		return userPassword;
+	}
+
 	public static Connection getConnection() {
 		if ( instance == null ) {
 			try {
