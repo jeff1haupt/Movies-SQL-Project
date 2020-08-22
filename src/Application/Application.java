2a4c492968856259b75a7870d8dc2700896146f9
zach beecher <zachbeecher@gmail.com>
2020-08-22 12:11:25 -0700

    merged

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -5,10 +5,8 @@ import java.sql.SQLException;
 public class Application {
 
 	public static void main(String[] args) throws SQLException {
-		
 		Menu menu = new Menu();
 		menu.start();
-	
 	}
 
 }
